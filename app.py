81343cb7a5e9a05f484ca7f0d58f3765f2fd7cd7
harshitha <80756064+harshitha04a@users.noreply.github.com>
2022-09-16 13:51:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,72 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 11 17:59:03 2022
+Created on Thu Sep 15 17:11:05 2022
 
 @author: harsh
 """
 
-import numpy as np
+
 import pickle
 import streamlit as st
-loaded_model=pickle.load(open("C:/Users/harsh/myfile.sav","rb"))
-def first(input_data):
-    input_array=np.asarray(input_data)
-    y=loaded_model.predict(input_array.reshape(1,-1))
-    return y
+
+  
+# loading in the model to predict on the data
+pickle_in = open('model.pkl', 'rb')
+classifier = pickle.load(pickle_in)
+  
+def welcome():
+    return 'welcome all'
+  
+# defining the function which will make the prediction using 
+# the data which the user inputs
+def prediction(year, km_driven, mileage, engine, max_power, age, Diesel, Electric, LPG, Petrol, Manual, seats5, more5seats):  
+   
+    prediction = classifier.predict(
+        [[year, km_driven, mileage, engine, max_power, age, Diesel, Electric, LPG, Petrol, Manual, seats5, more5seats]])
+    print(prediction)
+    return prediction
+      
+  
+# this is the main function in which we define our webpage 
 def main():
-    st.title("selling price prediction")
-    year=st.text_input("enter year")
-    kn_driven=st.text_input("driven_km")
-    mileage=st.text_input("mileage")
-    engine=st.text_input("engine")
-    max_power=st.text_input("max_power")
-    age=st.text_input("entr age")
-    diesel=st.radio("enter diesel or not","01")
-    if diesel==0:
-        st.write(0)
-    elif diesel==1:
-        st.write(1)
-    electric=st.radio("enter electric","01")
-    if electric==0:
-        st.write(0)
-    elif electric==1:
-        st.write(1)
-    lpg=st.radio("enter lpg","01")
-    if lpg==0:
-        st.write(0)
-    elif lpg==1:
-        st.write(1)
-    petrol=st.radio("enter petrol","01")
-    if petrol==0:
-        st.write(0)
-    elif petrol==1:
-        st.write(1)
-    manual=st.radio("enter manual","01")
-    if manual==0:
-        st.write(0)
-    elif manual==1:
-        st.write(1)
-    fiveseats=st.radio("enter 5 seats or not","01")
-    if fiveseats==0:
-        st.write(0)
-    elif fiveseats==1:
-        st.write(1)
-    gt5seats=st.radio("greater than 5 seats or not","01")
-    if gt5seats==0:
-        st.write(0)
-    elif gt5seats==1:
-        st.write(1)
-    diagnosis=''
-    if st.button("price prediction"):
-        diagnosis=first([float(year),float(kn_driven),float(mileage),float(engine),float(max_power),float(age),float(diesel),float(electric),float(lpg),float(petrol),float(manual),float(fiveseats),float(gt5seats)])
-    st.success(diagnosis)
+      # giving the webpage a title
+    st.title("Car Price Prediction")
+      
+    # here we define some of the front end elements of the web page like 
+    # the font and background color, the padding and the text to be displayed
+    html_temp = """
+    <div style ="background-color:yellow;padding:13px">
+    <h1 style ="color:black;text-align:center;">Classifier ML App </h1>
+    </div>
+    """
+      
+    # this line allows us to display the front end aspects we have 
+    # defined in the above code
+    st.markdown(html_temp, unsafe_allow_html = True)
+      
+    # the following lines create text boxes in which the user can enter 
+    # the data required to make the prediction
+    year = st.text_input("year", "Type Here")
+    km_driven = st.text_input("km_driven", "Type Here")
+    mileage = st.text_input("mileage", "Type Here")
+    engine = st.text_input("engine", "Type Here")
+    max_power = st.text_input("max_power", "Type Here")
+    age = st.text_input("age", "Type Here")
+    Diesel = st.text_input("Diesel", "Type Here")
+    Electric = st.text_input("Electric", "Type Here")
+    LPG = st.text_input("LPG", "Type Here")
+    Petrol = st.text_input("Petrol", "Type Here")
+    Manual = st.text_input("Manual", "Type Here")
+    seats5 = st.text_input("seats5", "Type Here")
+    more5seats = st.text_input(">5seats", "Type Here")
+    result =""
+      
+    # the below line ensures that when the button called 'Predict' is clicked, 
+    # the prediction function defined above is called to make the prediction 
+    # and store it in the variable result
+    if st.button("Predict"):
+        result = prediction(float(year), float(km_driven), float(mileage), float(engine), float(max_power), float(age), float(Diesel), float(Electric), float(LPG), float(Petrol), float(Manual), float(seats5), float(more5seats))
+    st.success('The output is {}'.format(result))
+     
 if __name__=='__main__':
     main()
